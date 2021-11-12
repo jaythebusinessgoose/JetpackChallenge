37a755277ed89483e8319eb0dd90d5c4cdcaaa42
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2021-11-11 21:05:07 -0700

    Several QOL changes: - Start the player with the jetpack equipped. - Do not count extra height gained when launched after death. - Added a visual indicator of the normal sunken city layer.

--- tia.lua
+++ tia.lua
@@ -39,7 +39,6 @@ tia.load_level = function()
         local euid = spawn_entity(ENT_TYPE.FLOOR_HORIZONTAL_FORCEFIELD, x, y, layer, 0, 0)
         local e = get_entity(euid)
         e.timer = 120
-        -- e.is_on = true
         e.flags = clr_flag(e.flags, ENT_FLAG.FACING_LEFT)
         return true
     end, "forcefield_left")
@@ -53,7 +52,6 @@ tia.load_level = function()
         local euid = spawn_entity(ENT_TYPE.FLOOR_HORIZONTAL_FORCEFIELD, x, y, layer, 0, 0)
         local e = get_entity(euid)
         e.timer = 60
-        -- e.is_on = false
         e.flags = set_flag(e.flags, ENT_FLAG.FACING_LEFT)
         return true
     end, "forcefield_right")
@@ -64,11 +62,12 @@ tia.load_level = function()
         return true
     end, "forcefield_right_top")
     local jetpack
-    level_state.callbacks[#level_state.callbacks+1] = set_pre_tile_code_callback(function(x, y, layer)
-        local euid = spawn_entity(ENT_TYPE.ITEM_JETPACK, x, y, layer, 0, 0)
+    level_state.callbacks[#level_state.callbacks+1] = set_callback(function()
+        if #players < 1 then return end
+        local euid = spawn_entity(ENT_TYPE.ITEM_JETPACK, 0, 0, LAYER.PLAYER, 0, 0)
         jetpack = get_entity(euid)
-        return true
-    end, "back_jetpack")
+        pick_up(players[1].uid, euid)
+    end, ON.LEVEL)
     local ground_level = 69.05
     local extra_height = 0
     local max_height = 0
@@ -90,7 +89,7 @@ tia.load_level = function()
         if #players < 1 then return end
         local player = players[1]
         local height = extra_height + player.y - ground_level
-        if height > max_height then
+        if height > max_height and player.health > 0 then
             max_height = height
             if height > overall_state.pb then
                 overall_state.pb = height
@@ -103,6 +102,27 @@ tia.load_level = function()
         local tw, th = draw_text_size(30, text)
         ctx:draw_text(0 - tw / 2, -0.935, 30, text, rgba(255, 255, 255, 195))
     end, ON.GUIFRAME)
+    level_state.callbacks[#level_state.callbacks+1] = set_callback(function(ctx, draw_depth)
+        if #players < 1 or extra_height > 0 then return end
+        if draw_depth == players[1].type.draw_depth then
+            for i=0,12 do
+                local column = 0
+                if i % 2 == 0 then
+                    column = 1
+                end
+                local rect2 = AABB:new(2.5 + i, 69 + 21.5, 3.5 + i, 69 + 20.5)
+
+                local rect3 = AABB:new(35 - (3.5 + i), 69 + 21.5, 35 - (2.5 + i), 69 + 20.5)
+                if i == 12 then
+                    ctx:draw_world_texture(TEXTURE.DATA_TEXTURES_FLOORMISC_0, 6, 5, rect2, Color:white())
+                    ctx:draw_world_texture(TEXTURE.DATA_TEXTURES_FLOORMISC_0, 6, 5, rect3, Color:white())
+                else
+                    ctx:draw_world_texture(TEXTURE.DATA_TEXTURES_BORDER_MAIN_0, 7, column, rect2, Color:white())
+                    ctx:draw_world_texture(TEXTURE.DATA_TEXTURES_BORDER_MAIN_0, 7, column, rect3, Color:white())
+                end
+            end
+        end
+    end, ON.RENDER_PRE_DRAW_DEPTH)
     level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity)
         entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
     end, SPAWN_TYPE.ANY, 0, ENT_TYPE.BG_LEVEL_SHADOW)
